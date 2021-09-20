ed17be4c6c5ba8fcd9e19aa042101531d80f0f1f
Igor <55404866+CepbluKot@users.noreply.github.com>
2021-09-20 11:03:33 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,52 +1,63 @@
 #include <iostream>
-using namespace std;
-
-
-class DArr {
+#include <stdlib.h>
+class Massiv {
+    
 public:
-	int mSize = 0;
-	DArr(int size) {
-		mSize = size;
-	}
-	
-	
+    int len;
+    Massiv(int lenParameter)
+    {   
+        len = lenParameter;
+    }
+    ~Massiv()
+    {
 
-	int* arr = new int[mSize];
-	
-	
-	void setArr(int ind, int val) {
-		if (val >= -100 && val <= 100) {
-			arr[ind] = val;
-		}
-	}
-	void print() {
-		
-		for (int i = 0; i < mSize; i++) {
-			cout << arr[i] << endl;
-			
-		}
-	}
-	bool getArr(int ind) {
-		if (ind < int(sizeof(arr)) / int(sizeof(int))) {
-			return true;
-		}
-		else {
-			return(false);
-		}
-		void coper() {
+    }
+    int* array = (int*) malloc (len * sizeof (int));
+    int* ptr = array;
 
-
+    void show() {
+        std::cout << ptr;
+    }
+    void setter(int num, int setterIndex) {
+        if (num >= -100 && num <= 100 && setterIndex < len) 
+        {
+            ptr[setterIndex] = num;
+            
+        }
+    }
+    void getter(int getterIndex) 
+    {
+        if (getterIndex < len) std::cout << ptr[getterIndex];
+    }
+    void copy(Massiv from, Massiv to)
+	{
+    	for(int i = 0; i < len; i++) to.ptr[i] = from.ptr[i];
 	}
+	void beyond(int num)
+	{	
+		if(num >= -100 && num <= 100)
+		{
+		len += 1;
+		array = (int*) realloc (array, len * sizeof (int));
+		ptr[len - 1] = num;
+	}
+	
 	}
 };
 
-
 int main()
 {
-
-	DArr array(2);
-	array.setArr(1, 1);
-	array.setArr(0, 0);
-	array.print();
-	
+    Massiv from(5);
+    Massiv to(5);
+    for (int i = 0; i < 5; i++) from.setter(i,i);
+    for (int i = 0; i < 5; i++) from.getter(i);
+    std::cout<<"\n";
+    to.copy(from, to);
+    for (int i = 0; i < 5; i++) to.getter(i);
+    std::cout<<"\n";
+    to.beyond(88);
+    for (int i = 0; i < 6; i++) to.getter(i);
+    
 }
+
+
